f8d788d0c1b8dccf113fec6c50b18ffddb43b577
lancelot <huxi@eptok.com>
2023-08-26 17:57:27 +0800

    add unit test

--- agent/agent-plugins/dubbo-alibaba/src/test/java/io/github/lancelothuxi/mock/agent/util/Person.java
+++ agent/agent-plugins/dubbo-alibaba/src/test/java/io/github/lancelothuxi/mock/agent/util/Person.java
@@ -18,6 +18,14 @@ public class Person {
         this.age = age;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }

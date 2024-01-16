70bff807319a661e36576d4149695457f61a02f4
huxi222 <huxi@eptok.com>
2024-01-16 17:31:25 +0800

    MockServerOperation

--- agent-core/src/main/java/io/github/lancelothuxi/mock/agent/polling/MockServerOperation.java
+++ agent-core/src/main/java/io/github/lancelothuxi/mock/agent/polling/MockServerOperation.java
@@ -4,17 +4,11 @@ import com.alibaba.fastjson.JSON;
 import io.github.lancelothuxi.mock.agent.config.GlobalConfig;
 import io.github.lancelothuxi.mock.agent.config.MockConfig;
 import io.github.lancelothuxi.mock.agent.config.registry.MockConfigRegistry;
-import io.github.lancelothuxi.mock.agent.core.MockAgent;
 import io.github.lancelothuxi.mock.agent.util.DigestUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import static io.github.lancelothuxi.mock.agent.config.Constant.MOCK_CONFIG_REGISTER_URL;
 import static io.github.lancelothuxi.mock.agent.config.Constant.QUERY_MOCK_CONFIG_LIST_URL;
@@ -26,11 +20,9 @@ public class MockServerOperation {
      * dubbo
      */
     public static void sync() {
+
         final QueryMockConfigsRequest queryMockConfigsRequest = register(
                 MockConfigRegistry.registryValues());
-
-        syncMockConfigsFromServer(queryMockConfigsRequest);
-
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
@@ -43,8 +35,7 @@ public class MockServerOperation {
 
     private static void syncMockConfigsFromServer(QueryMockConfigsRequest queryMockConfigsRequest) {
         final String result = HttpUtil.sendPostRequest(QUERY_MOCK_CONFIG_LIST_URL,queryMockConfigsRequest, 3000);
-        String md5Hex = DigestUtils
-                .getMD5(result).toUpperCase();
+        String md5Hex = DigestUtils.getMD5(result).toUpperCase();
         //数据有变化
         if (!md5Hex.equals(MockConfigRegistry.lastMd5)) {
             MockConfigRegistry.lastMd5 = md5Hex;
